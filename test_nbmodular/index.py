aaf94059701d32ba5b3ec0f912fbe97cd5e02bcc
Jaume Amores <jaume.dsdev@gmail.com>
2024-09-28 05:53:38 +0100

    Moved nbs to nbm and performed nbm_export

---
+++ test_nbmodular/index.py
@@ -0,0 +1,13 @@
+
+
+# %% auto 0
+__all__ = ['two_plus_three']
+
+# %% ../nbs/index.ipynb 1
+#@@function two_plus_three
+def two_plus_three(a, b, print, c):
+    a = 2
+    b = 3
+    c = a+b
+    print (f'The result of adding {a}+{b} is {c}')
+
